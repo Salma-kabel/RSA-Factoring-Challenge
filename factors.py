11bbab020c6e22e90c892736b4d66d3f966213ea
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-03 10:06:02 -0700

    answer

--- factors.py
+++ factors.py
@@ -9,17 +9,17 @@ def main():
             num = int(i)
             mod = 2
             if(num % mod == 0):
-                print("{}={}*{}".format(num, int(mod), int(num // mod)))
+                print("{}={}*{}".format(num, int(num // mod), int(mod)))
             else:
                 sqrt_ = int(math.sqrt(num)) + 1
                 for mod in range(3, sqrt_, 2):
                     if(num % mod == 0):
-                        print("{}={}*{}".format(num, int(mod), int(num // mod)))
+                        print("{}={}*{}".format(num,int(num // mod), int(mod)))
                         break
                     if(num % (sqrt_ + mod) == 0):
-                            print("{}={}*{}".format(num, int(sqrt_ + mod), int(num // (sqrt_ + mod))))
+                            print("{}={}*{}".format(num, int(num // (sqrt_ + mod)), int(sqrt_ + mod)))
                             break
                     if(num % (sqrt_ - mod) == 0):
-                        print("{}={}*{}".format(num, int(sqrt_ - mod), int(num // (sqrt_ - mod))))
+                        print("{}={}*{}".format(num, int(num // (sqrt_ - mod)), int(sqrt_ - mod)))
                         break
 main()
